d3309a88e4b02352526dc191c7317fb12b3a68ea
Johan Andersson <johan.rejeep@gmail.com>
2013-08-07 11:29:06 +0200

    Add ert-runner.

--- Cask
+++ Cask
@@ -1,1 +1,11 @@
+(source gnu)
+(source melpa)
+
 (package-file "ansi.el")
+
+(development
+ (depends-on "f")
+ (depends-on "s")
+ (depends-on "dash")
+ (depends-on "el-mock")
+ (depends-on "ert-runner"))
---
+++ test/ansi-init.el
@@ -0,0 +1,19 @@
+(require 'f)
+(require 's)
+(require 'dash)
+(require 'el-mock)
+(require 'ansi)
+
+(defvar ansi-test/test-path
+  (f-dirname load-file-name))
+
+(defvar ansi-test/root-path
+  (f-parent ansi-test/test-path))
+
+(defvar ansi-test/vendor-path
+  (f-expand "vendor" ansi-test/root-path))
+
+(unless (require 'ert nil 'noerror)
+  (require 'ert (f-expand "ert" ansi-test/vendor-path)))
+
+(add-to-list 'load-path ansi-test/root-path)
