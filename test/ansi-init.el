4c9ec0b7867248b829e0cf5250dfccb63ecad2e1
Johan Andersson <johan.rejeep@gmail.com>
2013-08-08 11:51:27 +0200

    Do not require ansi, load it.
    Otherwise ansi might already be loaded by some other package.

--- test/ansi-init.el
+++ test/ansi-init.el
@@ -2,7 +2,6 @@
 (require 's)
 (require 'dash)
 (require 'el-mock)
-(require 'ansi)
 
 (defvar ansi-test/test-path
   (f-dirname load-file-name))
@@ -12,6 +11,8 @@
 
 (defvar ansi-test/vendor-path
   (f-expand "vendor" ansi-test/root-path))
+
+(load (f-expand "ansi" ansi-test/root-path))
 
 (unless (require 'ert nil 'noerror)
   (require 'ert (f-expand "ert" ansi-test/vendor-path)))
